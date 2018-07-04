356d2e1095b1588f11e368dbfc0b824a9fd28c5f
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-04 16:12:28 +0100

    Minor changes

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,11 +4,13 @@ import (
 	"github.com/DomParfitt/gecko/lexer/token"
 )
 
+//Tokenize lexes a string into a list of
+// tokens
 func Tokenize(str string) []token.Token {
-	tokens := []token.Token{}
+	var tokens []token.Token
 	for _, ch := range str {
-		tokenType, err := token.Match(ch)
-		if err == nil {
+		tokenType, ok := token.Match(ch)
+		if ok {
 			tokens = append(tokens, token.Token{Token: tokenType, Value: ch})
 		}
 	}
