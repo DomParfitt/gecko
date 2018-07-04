4bd5c650b92d2379297eb50038949fe23c0f1d56
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-04 16:23:15 +0100

    Added return for parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,9 +1,10 @@
 package parser
 
 import (
+	"github.com/DomParfitt/gecko/automata"
 	"github.com/DomParfitt/gecko/lexer/token"
 )
 
-func Parse(tokens []token.Token) {
-
+func Parse(tokens []token.Token) *automata.FiniteState {
+	return automata.New()
 }
