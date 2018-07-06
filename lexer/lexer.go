6d81295b1360872de9f75fa0ac4d4ab721ab8545
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-06 13:22:47 +0100

    Added handling for invalid token

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,14 +1,19 @@
 package lexer
 
+import (
+	"fmt"
+)
+
 //Tokenize lexes a string into a list of
 // tokens
-func Tokenize(str string) []Token {
+func Tokenize(str string) ([]Token, error) {
 	var tokens []Token
-	for _, ch := range str {
+	for i, ch := range str {
 		tokenType, ok := Match(ch)
-		if ok {
-			tokens = append(tokens, Token{Type: tokenType, Value: ch})
+		if !ok {
+			return []Token{}, fmt.Errorf("unrecognised token: '%c' at position: %d", ch, i)
 		}
+		tokens = append(tokens, Token{Type: tokenType, Value: ch})
 	}
-	return tokens
+	return tokens, nil
 }
--- main.go
+++ main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/DomParfitt/gecko/compiler"
 	"github.com/DomParfitt/gecko/lexer"
+	"github.com/DomParfitt/gecko/parser"
 )
 
 func main() {
 	txt := "hello, gecko"
-	tokens := lexer.Tokenize(txt)
+	tokens, err := lexer.Tokenize(txt)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	for _, token := range tokens {
 		fmt.Println(token)
 	}
+	parser := parser.New()
+	tree, err := parser.Parse(tokens)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	exec := compiler.Compile(tree)
+	result := exec.Execute("hello")
+	fmt.Printf("%t", result)
 
 }
