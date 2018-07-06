f706416778bb909013e613aa85a8c2741213672b
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-06 12:31:33 +0100

    Updated to return AST instead of FSM

--- parser/parser.go
+++ parser/parser.go
@@ -1,8 +1,8 @@
 package parser
 
 import (
-	"github.com/DomParfitt/gecko/automata"
 	"github.com/DomParfitt/gecko/lexer"
+	"github.com/DomParfitt/gecko/stack"
 	"github.com/DomParfitt/gecko/tree"
 )
 
@@ -10,6 +10,7 @@ import (
 type Parser struct {
 	cursor int
 	tokens []lexer.Token
+	stack  *stack.Stack
 	tree   *tree.AbstractSyntax
 }
 
@@ -18,17 +19,24 @@ func New() *Parser {
 	return &Parser{
 		cursor: 0,
 		tokens: []lexer.Token{},
+		stack:  stack.New(),
 		tree:   nil,
 	}
 }
 
 // Parse a list of tokens into an executable
-func (p *Parser) Parse(tokens []lexer.Token) (*automata.FiniteState, error) {
+func (p *Parser) Parse(tokens []lexer.Token) (*tree.AbstractSyntax, error) {
 	p.cursor = 0
 	p.tokens = tokens
-	automata := automata.New()
 	//TODO: Logic
-	return automata, nil
+
+	for token, ok := p.consume(); ok; {
+		if p.literal(token) {
+
+		}
+	}
+
+	return p.tree, nil
 }
 
 // Consume the next token in the list and increment the cursor
@@ -37,7 +45,7 @@ func (p *Parser) consume() (lexer.Token, bool) {
 	// If cursor is incremented beyond final token then we
 	// can't consume any further so return failure
 	if p.cursor == len(p.tokens) {
-		return lexer.Token{Type: lexer.Digit, Value: ' '}, false
+		return lexer.Error(), false
 	}
 
 	token := p.tokens[p.cursor]
