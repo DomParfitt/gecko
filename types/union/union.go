a002644c7e42478d754a3c0b579749b35b2a1d21
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-10 12:51:39 +0100

    Renamed new function

--- types/union/union.go
+++ types/union/union.go
@@ -1,17 +1,20 @@
 package union
 
+//Union struct representing a sum type
 type Union struct {
 	this *interface{}
 	that *interface{}
 }
 
-func NewUnion(this, that *interface{}) Union {
+//New union
+func New(this, that *interface{}) Union {
 	return Union{
 		this: this,
 		that: that,
 	}
 }
 
+//Value of the union
 func (u Union) Value() *interface{} {
 	if u.this != nil {
 		return u.this
