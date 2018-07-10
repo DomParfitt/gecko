ecf1825abcddd03e25d9b986e164ddd8f144dd6d
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-10 12:58:22 +0100

    Added comments to grammars

--- parser/grammar.go
+++ parser/grammar.go
@@ -1,39 +1,47 @@
 package parser
 
+//RegExpr ::= Union | SimpleExpr
 type RegExpr struct {
 	union  *Union
 	simple *SimpleExpr
 }
 
+//Union ::= RegExpr "|" SimpleExpr
 type Union struct {
 	regex  *RegExpr
 	simple *SimpleExpr
 }
 
+//SimpleExpr ::= Concatenation | BasicExpr
 type SimpleExpr struct {
 	concatenation *Concatenation
 	basic         *BasicExpr
 }
 
+//Concatenation ::= SimpleExpr BasicExpr
 type Concatenation struct {
 	simple *SimpleExpr
 	basic  *BasicExpr
 }
 
+//BasicExpr ::= Star | Plus | Element
 type BasicExpr struct {
 	star    *Star
 	plus    *Plus
 	element *Element
 }
 
+//Star ::= Element "*"
 type Star struct {
 	element *Element
 }
 
+//Plus ::= Element "+"
 type Plus struct {
 	element *Element
 }
 
+//Element ::= Character
 type Element struct {
 	Value string
 }
