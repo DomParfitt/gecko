3c6d404d6895353b577990e008c6a2ecfaba0345
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-12 13:02:28 +0100

    Removed unused error return

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,11 +2,11 @@ package lexer
 
 //Tokenize lexes a string into a list of
 // tokens
-func Tokenize(str string) ([]Token, error) {
+func Tokenize(str string) []Token {
 	var tokens []Token
 	for _, ch := range str {
 		tokenType := Match(ch)
 		tokens = append(tokens, Token{Type: tokenType, Value: ch})
 	}
-	return tokens, nil
+	return tokens
 }
