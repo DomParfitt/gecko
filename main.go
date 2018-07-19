120237fec5312cd810039ea28d8032a5d8daa2ee
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-19 22:11:18 +0100

    Made main run the server

--- main.go
+++ main.go
@@ -1,28 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"github.com/DomParfitt/gecko/compiler"
-	"os"
-	"time"
+	"github.com/DomParfitt/gecko/server"
 )
 
 func main() {
-	start := time.Now()
-	args := os.Args[1:]
-	pattern := args[0]
-	input := args[1]
+	// args := os.Args[1:]
+	// pattern := args[0]
+	// input := args[1]
 
-	compiler := compiler.New()
-	result, err := compiler.MatchPattern(pattern, input)
+	// compiler := compiler.New()
+	// result, err := compiler.MatchPattern(pattern, input)
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-	fmt.Printf("%t\n", result)
-	end := time.Now()
-	fmt.Printf("Compiled the pattern %s in %f seconds\n", pattern, end.Sub(start).Seconds())
-
+	// fmt.Printf("%t\n", result)
+	server.Run()
 }
