dc98003a07dc199188aeb84e852c2b154532a084
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-30 15:18:31 +0100

    Experimenting with React frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Graph from 'react-graph-vis';
+
+class App extends Component {
+  render() {
+    var graph = {
+      nodes: [
+          {id: 1, label: 'Node 1'},
+          {id: 2, label: 'Node 2'},
+          {id: 3, label: 'Node 3'},
+          {id: 4, label: 'Node 4'},
+          {id: 5, label: 'Node 5'}
+        ],
+      edges: [
+          {from: 1, to: 2},
+          {from: 1, to: 3},
+          {from: 2, to: 4},
+          {from: 2, to: 5}
+        ]
+    };
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Graph graph={graph}/>
+      </div>
+    );
+  }
+}
+
+export default App;
