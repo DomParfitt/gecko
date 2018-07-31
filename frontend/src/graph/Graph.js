2b26ce77356e1848d5a6345b7db05d6df7fed8de
Dom Parfitt <dom.parfitt@gmail.com>
2018-07-31 14:12:22 +0100

    Added functionality to generate graph from state

--- frontend/src/graph/Graph.js
+++ frontend/src/graph/Graph.js
@@ -4,6 +4,18 @@ import {} from 'd3-graphviz';
 
 export default class Graph extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentNode: 0,
+            nodes: [
+                {id: 0, isTerminal: false},
+                {id: 1, isTerminal: true},
+            ],
+            edges: [{from: 0, to: 1, label: 'a'}]
+        };
+    }
+
     componentDidMount() {
         select('#graphDiv')
             .graphviz()
@@ -11,19 +23,41 @@ export default class Graph extends Component {
             .width(500)
             .fit(true)
             .zoom(false)
-            .renderDot(
-                `digraph { rankdir="LR"; 
-                    0 -> 1 [label="a"]; 
-                    0 -> 2 [label="b"]; 
-                    1 -> 0 [label="c"];
-                }`
-            );
+            .renderDot(this.generateDot());
     }
 
     render() {
-
         return(
             <div id="graphDiv" ref="graphDiv"></div>
         );
+    }
+
+    generateDot() {
+        let dot = 'digraph { rankdir="LR";\n';
+
+        for(let i = 0; i < this.state.nodes.length; i++) {
+            const node = this.state.nodes[i];
+            dot += node.id +' [';
+
+            if (node.isTerminal) {
+                dot += 'shape="doublecircle" ';
+            } else {
+                dot += 'shape="circle" ';
+            }
+
+            if (i === this.state.currentNode) {
+                dot += 'fillcolor="red" style="filled" ';
+            }
+
+            dot += '];\n';
+        }
+
+        for(let i = 0; i < this.state.edges.length; i++) {
+            const edge = this.state.edges[i];
+            dot += edge.from + '->' + edge.to + ' [label="' + edge.label + '"];\n';
+        }
+
+        dot += '}';
+        return dot;
     }
 }
