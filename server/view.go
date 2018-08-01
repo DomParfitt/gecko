a4a208ad675bfb277038da4e23d17d6845042370
Dom Parfitt <dom.parfitt@gmail.com>
2018-08-01 15:52:18 +0100

    Log change

--- server/view.go
+++ server/view.go
@@ -8,6 +8,6 @@ import (
 
 //ServeView on the given port
 func ServeView(port string) {
-	fmt.Printf("Listening on port %s...\n", port)
+	fmt.Printf("Serving view on port %s...\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, http.FileServer(http.Dir("./frontend/build/"))))
 }
