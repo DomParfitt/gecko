a102c2e6ec44017eb159c56f7c44625106ac75c2
Dom Parfitt <dom.parfitt@gmail.com>
2018-10-11 11:31:02 +0100

    Added ASTGraph component

---
+++ frontend/src/graph/ASTGraph.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class ASTGraph extends React.Component<any, any> {
+    public render(): JSX.Element {
+        return (
+            <div />
+        );
+    }
+}
+
+export default ASTGraph;
