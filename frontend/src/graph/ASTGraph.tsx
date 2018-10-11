cb1efe44ae61c864278eece45ce103415a73488a
Dom Parfitt <dom.parfitt@gmail.com>
2018-10-11 14:24:42 +0100

    Added AST rendering logic

--- frontend/src/graph/ASTGraph.tsx
+++ frontend/src/graph/ASTGraph.tsx
@@ -1,11 +1,71 @@
+import { graphviz } from 'd3-graphviz';
 import * as React from 'react';
+import { IAbstractSyntaxTree } from 'src/ast/AbstractSyntaxTree';
 
-class ASTGraph extends React.Component<any, any> {
+class ASTGraph extends React.Component<IASTGraphProps, any> {
+
+    private nodeCount: number;
+
+    constructor(props: IASTGraphProps) {
+        super(props);
+        this.nodeCount = 0;
+    }
+
     public render(): JSX.Element {
-        return (
-            <div />
+        return(
+            <div id="graphDiv" />
         );
     }
+
+    public componentDidMount() {
+        this.loadGraph();
+    }
+    
+    
+    public componentDidUpdate() {
+        this.loadGraph();
+    }
+
+    private loadGraph() {
+        graphviz('#graphDiv')
+            // .graphviz({useWorker: false})
+            .height(500)
+            .width(500)
+            .fit(true)
+            .zoom(false)
+            .renderDot(this.generateDot());
+    }
+    
+    private generateDot(): string {
+        let dot = 'digraph {\n';
+        dot += this.generateDotNode(this.props.ast);
+        dot += '}';
+        this.nodeCount = 0;
+        // tslint:disable-next-line:no-console
+        console.log(dot);
+        return dot;
+    }
+
+    private generateDotNode(ast: IAbstractSyntaxTree): string {
+        const rootName = 'node' + this.nodeCount;
+        let dot = rootName +  ' [label="' + ast.label + '"]\n';
+        if (ast.children.length === 0) {
+            return dot;
+        } 
+
+        for (const child of ast.children) {
+            this.nodeCount++;
+            const childName = 'node' + this.nodeCount;
+            dot += this.generateDotNode(child);
+            dot += rootName + ' -> ' + childName + '\n';
+        }
+
+        return dot;
+    }
+}
+
+export interface IASTGraphProps extends React.ClassAttributes<ASTGraph> {
+    ast: IAbstractSyntaxTree
 }
 
 export default ASTGraph;
