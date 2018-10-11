0351ffdd9d375ea3b5ffbf176cf3c7d347fc3648
Dom Parfitt <dom.parfitt@gmail.com>
2018-10-11 16:11:24 +0100

    Added stubs

--- core/parser/transformer.go
+++ core/parser/transformer.go
@@ -7,3 +7,83 @@ import (
 type Transformer interface {
 	transform() api.AST
 }
+
+func (r *RegExpr) transform() api.AST {
+	ast := api.AST{
+		Label:    "RegExpr",
+		Children: []api.AST{},
+	}
+	return ast
+}
+
+func (u *Union) transform() api.AST {
+
+}
+
+func (s *SimpleExpr) transform() api.AST {
+
+}
+
+func (c *Concatenation) transform() api.AST {
+
+}
+
+func (b *BasicExpr) transform() api.AST {
+
+}
+
+func (s *Star) transform() api.AST {
+
+}
+
+func (p *Plus) transform() api.AST {
+
+}
+
+func (q *Question) transform() api.AST {
+
+}
+
+func (e *Element) transform() api.AST {
+
+}
+
+func (g *Group) transform() api.AST {
+
+}
+
+func (e *Escape) transform() api.AST {
+
+}
+
+func (s *Set) transform() api.AST {
+
+}
+
+func (p *PositiveSet) transform() api.AST {
+
+}
+
+func (n *NegativeSet) transform() api.AST {
+
+}
+
+func (s *SetItems) transform() api.AST {
+
+}
+
+func (s *SetItem) transform() api.AST {
+
+}
+
+func (r *Range) transform() api.AST {
+
+}
+
+func (c *Character) transform() api.AST {
+
+}
+
+func (b *Base) transform() api.AST {
+
+}
