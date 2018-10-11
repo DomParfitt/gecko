fd974b54feffd84b456e3f2980854a5f804a12a9
Dom Parfitt <dom.parfitt@gmail.com>
2018-10-11 11:31:33 +0100

    Updated components. Added debugMode for logging

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,98 +1,107 @@
 import * as React from 'react';
 import './App.css'
 import { IAutomata, isAutomata } from './automata/Automata';
-import Graph from './graph/Graph';
+import GraphsHolder from './graph/GraphsHolder';
+import TextInput from './input/TextInput';
 
-class App extends React.Component<any, IAppState> {
+class App extends React.Component<IAppProps, IAppState> {
 
-  constructor(props: any) {
-    super(props);
+    constructor(props: any) {
+        super(props);
 
-    this.state = {
-      ast: {},
-      automata: {
-        currentNode: 0,
-        edges: [
-          { from: 0, to: 1, label: 'a' },
-          { from: 1, to: 2, label: 'b' },
-          { from: 2, to: 1, label: 'c' },
-        ],
-        nodes: [
-          { id: 0, isTerminal: false },
-          { id: 1, isTerminal: true },
-          { id: 2, isTerminal: false },
-        ],
-      },
-      input: "",
-      matches: false,
-      pattern: "abc",
-    };
+        this.state = {
+            ast: {},
+            automata: {
+                currentNode: 0,
+                edges: [
+                    { from: 0, to: 1, label: 'a' },
+                    { from: 1, to: 2, label: 'b' },
+                    { from: 2, to: 1, label: 'c' },
+                ],
+                nodes: [
+                    { id: 0, isTerminal: false },
+                    { id: 1, isTerminal: true },
+                    { id: 2, isTerminal: false },
+                ],
+            },
+            input: "",
+            matches: false,
+            pattern: "abc",
+        };
 
-  }
+    }
 
-  public render(): JSX.Element {
-    return (
-      <div className="App">
-        <h1>Welcome to Gecko!</h1>
-        <div>
-          <input type="text" placeholder="Enter a pattern" onChange={
-            // tslint:disable-next-line:jsx-no-lambda
-            (event) => {
-              const pattern = event.target.value;
-              this.setState({'pattern': pattern});
-              this.requestAutomata(pattern);
-            }
-          }/>
-          <button onClick={
-            // tslint:disable-next-line:jsx-no-lambda
-            () => this.requestAutomata(this.state.pattern)
-            } >Enter</button>
-        </div>
-        <div>
-          <input type="text" placeholder="Enter an input" />
-          <button>Enter</button>
-        </div>
-        <div>Pattern: {this.state.pattern}</div>
-        <div>Input: {this.state.input}</div>
-        <div>Matches: {this.state.matches.toString()}</div>
-        <Graph currentNode={this.state.automata.currentNode} nodes={this.state.automata.nodes} edges={this.state.automata.edges} />
-      </div>
-    );
-  }
+    public render(): JSX.Element {
+        return (
+            <div className="App">
+                <h1>Welcome to Gecko!</h1>
+                <TextInput placeholder="Enter a pattern" onChangeHandler={this.handlePatternChange} onClickHandler={this.handlePatternClick} hideButton={true} />
+                <TextInput placeholder="Enter an input" onChangeHandler={this.handleInputChange} />
+                <div>Pattern: {this.state.pattern}</div>
+                <div>Input: {this.state.input}</div>
+                <div>Matches: {this.state.matches.toString()}</div>
+                {/* <AutomataGraph automata={this.state.automata} /> */}
+                <GraphsHolder automata={this.state.automata} ast={this.state.ast}/>
+            </div>
+        );
+    }
 
-  private requestAutomata(pattern: string) {
-    // tslint:disable-next-line:no-console
-    console.log(pattern);
-    fetch("http://localhost:8080/pattern/" + encodeURI(pattern))
-      .then((resp) => resp.json())
-      .then(
-        (data) => {
-          // tslint:disable-next-line:no-console
-          console.log(data);
-          if (isAutomata(data)) {
-            this.handleAutomataData(data);
-          }
-        },
-        (error) => {
-          // tslint:disable-next-line:no-console
-          console.log("Gecko Server Unavailable.", error)
+    private handlePatternChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const pattern = event.target.value;
+        this.setState({ 'pattern': pattern });
+        this.requestAutomata(pattern);
+    }
+
+    private handlePatternClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        this.requestAutomata(this.state.pattern);
+    }
+
+    private handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const input = event.target.value;
+        this.setState({ 'input': input });
+    }
+
+    private requestAutomata(pattern: string) {
+        this.log(pattern);
+        fetch("http://localhost:8080/pattern/" + encodeURI(pattern))
+            .then((resp) => resp.json())
+            .then(
+                (data) => {
+                    this.log(data);
+                    if (isAutomata(data)) {
+                        this.handleAutomataData(data);
+                    }
+                },
+                (error) => {
+                    this.log("Gecko Server Unavailable. " + error)
+                }
+            );
+    }
+
+    private handleAutomataData(automata: IAutomata) {
+        automata.nodes.sort();
+        this.setState({ "automata": automata });
+    }
+
+    private log(message: string) {
+        if (this.props.debugMode) {
+            // tslint:disable-next-line:no-console
+            console.log(message);
         }
-      );
-  }
-
-  private handleAutomataData(automata: IAutomata) {
-    automata.nodes.sort();
-    this.setState({"automata": automata});
-  }
+    }
 
 }
 
+export interface IAppProps extends React.ClassAttributes<App> {
+    debugMode: boolean;
+}
+
 export interface IAppState extends React.ComponentState {
-  input: string,
-  pattern: string,
-  matches: boolean,
-  ast: any,
-  automata: IAutomata 
+    input: string,
+    pattern: string,
+    matches: boolean,
+    ast: any,
+    automata: IAutomata
 }
 
 export default App;
