0781e6e7b6b9011d81ebbcdeb1b59ed5324632f6
Dom Parfitt <dom.parfitt@gmail.com>
2018-10-26 15:18:43 +0100

    Added optional options prop and default options

--- frontend/src/graph/Graphviz.tsx
+++ frontend/src/graph/Graphviz.tsx
@@ -1,9 +1,16 @@
-import { graphviz } from 'd3-graphviz';
+import { graphviz, GraphvizOptions } from 'd3-graphviz';
 import * as React from 'react';
 
 class Graphviz extends React.Component<IGraphvizProps, any> {
-    
+
     private static count = 0;
+    private static defaultOptions: GraphvizOptions = {
+        fit: true,
+        height: 500,
+        width: 500,
+        zoom: false,
+    };
+
     private id: string;
 
     constructor(props: IGraphvizProps) {
@@ -13,7 +20,7 @@ class Graphviz extends React.Component<IGraphvizProps, any> {
     }
 
     public render(): JSX.Element {
-        return(
+        return (
             <div id={this.id} />
         );
     }
@@ -21,25 +28,22 @@ class Graphviz extends React.Component<IGraphvizProps, any> {
     public componentDidMount() {
         this.loadGraph();
     }
-    
-    
+
     public componentDidUpdate() {
         this.loadGraph();
     }
 
     private loadGraph() {
         graphviz('#' + this.id)
-            .height(500)
-            .width(500)
-            .fit(true)
-            .zoom(false)
+            .options(this.props.options || Graphviz.defaultOptions)
             .renderDot(this.props.dot);
     }
 
 }
 
 export interface IGraphvizProps extends React.ClassAttributes<Graphviz> {
-    dot: string
+    dot: string,
+    options?: GraphvizOptions,
 }
 
 export default Graphviz;
