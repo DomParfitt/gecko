cd1fbdd61d2beea3bcbccde5b5e7c8292c3a9b44
Dom Parfitt <>
2018-11-02 18:10:10 +0000

    Implemented stepping

--- frontend/src/graph/AutomataViewer.tsx
+++ frontend/src/graph/AutomataViewer.tsx
@@ -6,9 +6,9 @@ export class AutomataViewer extends React.Component<IAutomataViewerProps, IAutom
 
     constructor(props: IAutomataViewerProps) {
         super(props);
-        this.state = { 
+        this.state = {
             currentNode: 0,
-            flattenEdges: false, 
+            flattenEdges: false,
         };
     }
 
@@ -21,33 +21,61 @@ export class AutomataViewer extends React.Component<IAutomataViewerProps, IAutom
                     <input type="checkbox" onChange={this.setFlattenEdges} />
                     </label>
                 </div>
-                <button onClick={this.stepBackward}>Back</button>
-                <button onClick={this.stepForward}>Forward</button>
-                <AutomataGraph automata={this.props.automata} currentNode={this.state.currentNode} flattenEdges={this.state.flattenEdges} />
+                <button disabled={this.isBackwardButtonDisabled()} onClick={this.stepBackward}>Back</button>
+                <button disabled={this.isForwardButtonDisabled()} onClick={this.stepForward}>Forward</button>
+                <AutomataGraph
+                    automata={this.props.automata}
+                    currentNode={this.props.steps ? this.props.steps[this.state.currentNode] : undefined}
+                    previousNode={this.state.previousNode}
+                    flattenEdges={this.state.flattenEdges}
+                />
             </div>
         );
     }
+
 
     private setFlattenEdges = (event: React.ChangeEvent<HTMLInputElement>) => {
         this.setState({ flattenEdges: !this.state.flattenEdges });
     }
 
     private stepForward = (event: React.MouseEvent<HTMLButtonElement>) => {
-        this.setState({ currentNode: this.state.currentNode + 1 });
+        this.setState({
+            currentNode: this.state.currentNode + 1,
+            previousNode: this.props.steps ? this.props.steps[this.state.currentNode] : undefined,
+        });
     }
 
     private stepBackward = (event: React.MouseEvent<HTMLButtonElement>) => {
-        this.setState({ currentNode: this.state.currentNode - 1 }); 
+        this.setState({
+            currentNode: this.state.currentNode - 1,
+            previousNode: this.props.steps ? this.props.steps[this.state.currentNode] : undefined,
+        });
+    }
+
+    private isForwardButtonDisabled(): boolean {
+        if (!this.props.steps) {
+            return true;
+        }
+        return this.state.currentNode >= this.props.steps.length - 1;
+    }
+
+    private isBackwardButtonDisabled(): boolean {
+        if (!this.props.steps) {
+            return true;
+        }
+
+        return this.state.currentNode <= 0;
     }
 
 }
 
 export interface IAutomataViewerProps extends React.ClassAttributes<AutomataViewer> {
-    automata: IAutomata
+    automata: IAutomata,
+    steps?: number[],
 }
 
-// tslint:disable-next-line:no-empty-interface
 export interface IAutomataViewerState extends React.ComponentState {
     currentNode: number,
-    flattenEdges: boolean
+    flattenEdges: boolean,
+    previousNode?: number,
 }
